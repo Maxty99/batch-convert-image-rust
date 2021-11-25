50b5f4138bcd847b88abe3e772ac390670c8dbd0
Max T <maxtyuterev@gmail.com>
2021-11-25 14:06:00 -0500

    Should work but haven't tested

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{env, fs};
 use threadpool::ThreadPool;
 
 // TODO: Add more types later
-const SUPPORTED_TYPES: [&str; 4] = ["JPG", "PNG", "TIFF", "JPEG"];
+// const SUPPORTED_TYPES: [&str; 4] = ["JPG", "PNG", "TIFF", "JPEG"];
 
 //TODO: Get rid of all those unessesary unwraps!!!
 
@@ -45,103 +45,80 @@ fn thread_convert(paths: Vec<String>, new_ext: String) {
     }
 }
 
-fn verify_args(args: &Vec<String>) -> bool {
-    // Make sure the args are long enough
-    if args.len() < 3 {
-        return false;
-    }
-
-    // I want to skip the first element
-    for arg in args.get(1..) {
-        let mut found: bool = false;
-        for t in SUPPORTED_TYPES {
-            // Dereference arg to see what it points to
-            if *arg[0] == t.to_uppercase() {
-                found = true;
-                break;
-            }
-        }
-        if !found {
-            eprintln!("That format is not supported");
-            return false;
-        }
-    }
-
-    return true;
-}
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
     let yaml = clap::load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
-
-    if verify_args(&args) {
-        let in_formats = args.get(2..).unwrap();
-
-        let dir = match env::current_dir() {
-            Ok(dir) => dir,
-            Err(error) => panic!(
-                "Problem opening current directory, possibly due to lack of privileges: {:?}",
-                error
-            ),
+    let app = App::from_yaml(yaml);
+    let matches = app.get_matches();
+    let num_of_threads: usize;
+    if matches.is_present("THREADS") {
+        let num_of_threads_str = matches.value_of("THREADS").unwrap();
+        num_of_threads = match num_of_threads_str.parse::<usize>() {
+            Ok(num) => num,
+            Err(_error) => {
+                panic!("Threads not a number");
+            }
         };
-        // Makes sense
-        let paths = match fs::read_dir(&dir) {
-            Ok(paths) => paths,
-            Err(error) => panic!(
-                "Problem opening current directory, possibly due to lack of privileges: {:?}",
-                error
-            ),
-        };
+    } else {
+        num_of_threads = 8;
+    };
+    let in_formats: Vec<&str> = matches.values_of("CONVERT_FROM").unwrap().collect(); //Can unwrap because it is required
 
-        // What the heck
-        let mut file_names_as_string =
+    let dir = match env::current_dir() {
+        Ok(dir) => dir,
+        Err(error) => panic!(
+            "Problem opening current directory, possibly due to lack of privileges: {:?}",
+            error
+        ),
+    };
+    // Makes sense
+    let paths = match fs::read_dir(&dir) {
+        Ok(paths) => paths,
+        Err(error) => panic!(
+            "Problem opening current directory, possibly due to lack of privileges: {:?}",
+            error
+        ),
+    };
+
+    // What the heck
+    let mut file_names_as_string =
             // Filter map to get rid of err
             paths.filter_map(|entry| {
-                entry.ok().and_then(|entry| {
-                    entry.path().to_str().and_then(|entry| {
+                entry.ok()
+                .and_then(|entry| {
+                    entry.path().to_str()
+                    .and_then(|entry| {
                         //Convert to string and use some because it yells at me if I dont
                         Some(String::from(entry))
                     })
                 })
             }).collect::<Vec<String>>();
 
-        file_names_as_string.retain(|file_path| {
-            let mut desired_format = false;
-            for format in in_formats {
-                // To avoid getting folders that happen to
-                let mut ext = format.to_lowercase();
-                ext.insert_str(0, ".");
-                if file_path.ends_with(&ext) {
-                    desired_format = true
-                }
+    file_names_as_string.retain(|file_path| {
+        let mut desired_format = false;
+        for format in &in_formats {
+            // To avoid getting folders that happen to be captured by read_dir
+            let mut ext = format.to_lowercase();
+            ext.insert_str(0, ".");
+            if file_path.ends_with(&ext) {
+                desired_format = true
             }
-
-            desired_format
-        });
-        // Threads hardcoded for now
-        let pool = ThreadPool::new(8);
-
-        let file_names_chunked = file_names_as_string.chunks(8);
-
-        // let mut handles: Vec<JoinHandle<()>> = Vec::new();
-        for file_name_chunk in file_names_chunked {
-            let owned_chunk_vec = file_name_chunk.to_vec();
-            let out_format = args.get(1).unwrap().to_owned().to_lowercase();
-            pool.execute(move || thread_convert(owned_chunk_vec, out_format));
-            // handles.push(handle);
         }
 
-        pool.join();
-    } else {
-        println!(
-            "
-        Usage : convert formatToConvertTo formatToConvertFrom ...
+        desired_format
+    });
 
-        To use this script you must pass the format you want to convert
-        as the first argument without the dot before it(PNG, JPG). Then list
-        the formats you are trying to convert from.
-        "
-        )
+    // Threads hardcoded for now
+    let pool = ThreadPool::new(num_of_threads);
+
+    let file_names_chunked = file_names_as_string.chunks(num_of_threads);
+
+    // let mut handles: Vec<JoinHandle<()>> = Vec::new();
+    for file_name_chunk in file_names_chunked {
+        let owned_chunk_vec = file_name_chunk.to_vec();
+        let out_format = String::from(matches.value_of("CONVERT_TO").unwrap());
+        pool.execute(move || thread_convert(owned_chunk_vec, out_format));
+        // handles.push(handle);
     }
+
+    pool.join();
 }
